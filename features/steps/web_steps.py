941ce7f464a56d5962f0f1ad13b5808684371cf2
Vladyslava I <yurchuk.vl@gmail.com>
2025-02-26 22:16:36 +0000

    Lab changes

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -132,3 +132,8 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
